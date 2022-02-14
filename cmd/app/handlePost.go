771d20d1c15193b0ad7af24b3272f1d120318a77
Dilog78 <sport777life@gmail.com>
2022-02-15 00:03:27 +0200

    added swagger

--- cmd/app/handlePost.go
+++ cmd/app/handlePost.go
@@ -9,6 +9,19 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// ShowAccount godoc
+// @Summary      Show an account
+// @Description  get string by ID
+// @Tags         accounts
+// @Accept       json
+// @Produce      json
+// @Param        id   path      int  true  "Account ID"
+// @Success      200  {object}  model.Account
+// @Failure      400  {object}  httputil.HTTPError
+// @Failure      404  {object}  httputil.HTTPError
+// @Failure      500  {object}  httputil.HTTPError
+// @Router       /accounts/{id} [get]
+
 func getPostJson(c echo.Context) error {
 	id, _ := strconv.Atoi(c.QueryParam("id"))
 
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+// @title Post app with authorization
+// @version 1.0
+// @description Educational project
+
+// @contact.email evgen.myroshnykov@gmail.com
+
+// @host localhost:8080
+// @BasePath /
+// @schemes http
 
 func main() {
 	e := echo.New()
 
 	routs(e)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
